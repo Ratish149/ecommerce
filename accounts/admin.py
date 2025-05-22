c884ae892f5e6b46c5098ac295803e92792fb4de
Ratish149 <ratish.shakya149@gmail.com>
2025-05-22 16:14:29 +0545

    dfd

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import User
+from unfold.admin import ModelAdmin
 # Register your models here.
+
+
+class UserAdmin(ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+
+
+admin.site.register(User, UserAdmin)
