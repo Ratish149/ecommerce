66c3995a04cda05dfb2508fb75a2dc5e2e8eb3be
bibek1414 <bbhattarai770@gmail.com>
2025-05-31 15:32:37 +0545

    url position changed

--- products/urls.py
+++ products/urls.py
@@ -9,10 +9,10 @@ from .views import (
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
-    path('products/<slug:category_slug>/<slug:slug>/',
-         ProductDetailView.as_view(), name='product-detail'),
     path('products/<slug:slug>/similar/',
          SimilarProductsView.as_view(), name='similar-products'),
+    path('products/<slug:category_slug>/<slug:slug>/',
+         ProductDetailView.as_view(), name='product-detail'),
     path('categories/', CategoryListCreateView.as_view(),
          name='category-list-create'),
     path('categories/<slug:slug>/', CategoryDetailView.as_view(),
