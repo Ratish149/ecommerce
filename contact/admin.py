fc4bc92dd098279d2c5c4c9f23e54b80821170a5
Ratish149 <ratish.shakya149@gmail.com>
2025-06-03 13:20:26 +0545

    dfd

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from unfold.admin import ModelAdmin
+from .models import Contact
 # Register your models here.
+
+
+class ContactAdmin(ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'created_at')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('created_at',)
+
+
+admin.site.register(Contact, ContactAdmin)
