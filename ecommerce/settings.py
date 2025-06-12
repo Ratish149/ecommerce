f41d300ed4b919ad4bd9af2508aaccbca1b2638b
Ratish149 <ratish.shakya149@gmail.com>
2025-06-12 16:33:38 +0545

    dfd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,12 +122,24 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecom',
+        'USER': 'ratish',
+        'PASSWORD': 'ratish123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
+
 AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
