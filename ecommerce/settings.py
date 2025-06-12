00d4297020ca21f957504bd08a4eb1d7725eace5
Ratish149 <ratish.shakya149@gmail.com>
2025-06-12 16:52:28 +0545

    dfd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -182,9 +182,9 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR, 'media')
 
